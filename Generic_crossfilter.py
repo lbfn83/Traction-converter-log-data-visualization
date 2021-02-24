ab9e518399dbfd09e1bebb873173f8181abc4bc0
wonjae <lbfn83@gmail.com>
2021-02-23 23:08:18 -0500

    Make progress/ Now selectedData can be shared across all of graphs regardless of which graph is clicked for selection

--- Generic_crossfilter.py
+++ Generic_crossfilter.py
@@ -3,21 +3,33 @@ import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
 import pandas as pd
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
+
+# Things to solve
+# ex) select 1, 2 in graph1
+# then select 3 in graph2
+# then go back to graph1 and select 4 => 1,2,4 are selected
+# Which means there must be a buffer that keeps all the selected points
+# even if selectedData callback event becomes None.
+# Locate this buffer and find the way to reset this buffer as well
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
 app.config['suppress_callback_exceptions'] = True
+
 
 # make a sample data frame with 6 columns
 np.random.seed(0)
 df = pd.DataFrame({"Col " + str(i+1): np.random.rand(30) for i in range(6)})
 
+loop_braker_container = html.Div(id='loop_breaker_container', children=[])
+
 app.layout = html.Div([
     html.Div(
-        dcc.Graph(id='g1', config={'displayModeBar': False}),
+        dcc.Graph(id='g1',  config={'displayModeBar': False}),
         className='four columns'
     ),
     html.Div(
@@ -27,12 +39,15 @@ app.layout = html.Div([
     html.Div(
         dcc.Graph(id='g3', config={'displayModeBar': False}),
         className='four columns'
-    )
+    ),
+
+    #This is registered to prevent the potential circular callback
+    loop_braker_container,
 ], className='row')
 
 def get_figure(df, x_col, y_col, selectedpoints, selectedpoints_local):
-    #drag 해서 생성된 사각형 범주를 가리키는 건데... 이건 내가 만들려는 것과 
-    #관계가 없기 때문에... 삭제 가능
+    #  # The below code is to draw the ractangle line over the range of selected points
+    # Can be utilized to mark the area where the points are selected
 
     # if selectedpoints_local and selectedpoints_local['range']:
     #     ranges = selectedpoints_local['range']
@@ -69,33 +84,47 @@ def get_figure(df, x_col, y_col, selectedpoints, selectedpoints_local):
     [Output('g1', 'figure'),
      Output('g2', 'figure'),
      Output('g3', 'figure'),
+     Output('loop_breaker_container', 'children')
      ],
     [Input('g1', 'selectedData'),
      Input('g2', 'selectedData'),
-     Input('g3', 'selectedData')]
+     Input('g3', 'selectedData')],
+    [State('loop_breaker_container', 'children')]
+
 )
-def callback(selection1, selection2, selection3):
+def CB_selectedData_Update(selection1, selection2, selection3, loop_breaker_container  ):
     selectedpoints = df.index
-    #for loop here  / element of list is extracted one by one  / the premise here is that previous selected value is not returned
-    #but only current one would be returned/ this is proved to be worng.
-    #selection2 update -> selection1 update / 이 경우 selection2 만 반영되는 문제가 발생한다.
-    #time stamp comparison should be implemented / hidden input을 만들어 계속 여기를 update해주고..
-    #그것 보다 최근의 것만 반영하도록 해줘야 한다.
 
-    #이걸 for문으로 돌리는게 문제인듯 / intersection..
-    for selected_data in [selection1, selection2, selection3]:
-        if selected_data and selected_data['points']:
-            selectedpoints = np.intersect1d(selectedpoints,
-                [p['customdata'] for p in selected_data['points']])
+    if all([selection1 == None, selection2 == None, selection3 == None, loop_breaker_container != []]):
 
-    return [get_figure(df, "Col 1", "Col 2", selectedpoints, selection1),
-            get_figure(df, "Col 3", "Col 4", selectedpoints, selection2),
-            get_figure(df, "Col 5", "Col 6", selectedpoints, selection3),
-            None,
-            None,
-            None,
-            ]
+        return [dash.no_update, dash.no_update, dash.no_update, []]
 
+    else:
+
+        #이걸 for문으로 돌리는게 문제인듯 / intersection..
+        for selected_data in [selection1, selection2, selection3]:
+            if selected_data and selected_data['points']:
+                selectedpoints = np.intersect1d(selectedpoints,
+                    [p['customdata'] for p in selected_data['points']])
+
+
+
+        return [get_figure(df, "Col 1", "Col 2", selectedpoints, selection1),
+                get_figure(df, "Col 3", "Col 4", selectedpoints, selection2),
+                get_figure(df, "Col 5", "Col 6", selectedpoints, selection3),
+                [html.Div(id='loop_breaker')]]
+
+@app.callback(
+    [Output('g1', 'selectedData'),
+     Output('g2', 'selectedData'),
+     Output('g3', 'selectedData'),
+
+     ],
+    [Input('loop_breaker', 'children')]
+
+)
+def CB_following_figure_Update(*input):
+    return [None, None, None]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
